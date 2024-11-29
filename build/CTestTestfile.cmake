6eadde83f36374491d56697f97fa4d2ee1dbb6c2
Joao Mendes <joao.felix.mendes@tecnico.ulisboa.pt>
2024-11-29 13:28:50 +0000

    merge submodule motion_planning

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joaomendes/workspace1/src/motion_planning_python
-# Build directory: /home/joaomendes/workspace1/src/motion_planning_python/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
